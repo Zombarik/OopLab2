82230cc922e8733cfd772d5ce0ab64d57c9b63b1
Zombarik <95669146+Zombarik@users.noreply.github.com>
2021-12-07 09:19:20 +0200

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,72 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QTime>
+#include <QLabel>
+#include <QVector>
+#include <QSoundEffect>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+struct TimeLeft {
+    int hours;
+    int minutes;
+    int seconds;
+    int sound_number;
+    bool was_played = false;
+};
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QTimer *timerForTimer;
+    QTimer *timerForAlarm;
+    QTimer *timerForMainClock;
+
+    QVector<TimeLeft*> timeLeft;
+    QVector<QLabel*> labels_timer;
+    int iForGrid_timer;
+    int jForGrid_timer;
+
+    QVector<TimeLeft*> alarmLeft;
+    QVector<QLabel*> labels_alarm;
+    int iForGrid_alarm;
+    int jForGrid_alarm;
+
+    QSoundEffect  melody;
+ int activeTimersCount = 0;
+
+
+
+private slots:
+    void updateTimer();
+    void updateAlarm();
+    void start_clock();
+    int reduceTime(TimeLeft* timeLeft);
+    void on_finish(TimeLeft* obj);
+    void setSoundNumber(TimeLeft* obj);
+
+    void on_createTimer_clicked();
+   // void updateStatusBar(const QTime& closestTimer);
+
+    void on_stopSound_clicked();
+
+    void on_startTimer_clicked();
+    void on_startAlarm_clicked();
+    void on_createAlarm_clicked();
+
+};
+
+#endif // MAINWINDOW_H
